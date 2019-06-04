3da5d63fd564e0706e21fdebf06679088fdace98
Christian <christianperss@gmail.com>
2019-06-04 21:16:08 +0200

    updates

--- src/index.js
+++ src/index.js
@@ -8,6 +8,39 @@ import UserWindow from './js/UserWindow';
 class Index {
 	constructor() {
 
+		const db = firebase.firestore();
+
+		console.log(db);
+
+
+		const getDrawings = (username) => {
+			const drawingsRef = db.collection("Users").doc(username).collection("Drawings");
+			drawingsRef.get().then(function(querySnapshot) {
+				querySnapshot.forEach(function(doc) {
+					// doc.data() is never undefined for query doc snapshots
+					console.log(doc.id, " => ", doc.data());
+				});
+			});
+		};
+
+		const getDataTest = (username) => {
+			const docRef = db.collection("Users").doc(username);
+			var getOptions = {
+				source: ''
+			};
+			docRef.get().then(function(doc) {
+				if (doc.exists) {
+					console.log("Document data:", doc.data());
+					getDrawings(username);
+				} else {
+					// doc.data() will be undefined in this case
+					console.log("No such document!");
+				}
+			}).catch(function(error) {
+				console.log("Error getting document:", error);
+			});
+		}
+
 		this.signIn = new SignIn();
 		this.userWindow = new UserWindow(document.body);
 
@@ -18,6 +51,7 @@ class Index {
 				this.signIn.hide();
 
 				const { email } = user;
+				getDataTest(email);
 				this.userWindow.update(email);
 				this.userWindow.show();
 				if (this.starter) {
